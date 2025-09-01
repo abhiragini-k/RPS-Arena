9743a6ffe75de4653d1cf0a1f23d53581a4789b9
abhiragini-k <abhiraginik@gmail.com>
2025-09-01 20:23:46 +0530

    01 Sept 2025

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 
-userScore=0
+'''userScore=0
 botScore=0
 drawScore=0
 
@@ -50,13 +50,13 @@ def play():
 
 
 def numberOfRounds():
-    print("Best out of how many times")
-    print("press 1 for best out of 3")
-    print("press 2 for best out of 5")
-    print("press 3 for best out of 7")
-    print("press 4 for custom number of rounds")
+    print("Choose series length (best of N, N must be odd):")
+    print("1) Best of 3")
+    print("2) Best of 5")
+    print("3) Best of 7")
+    print("4) Custom odd number")
 
-    n = int(input())
+    n = int(input("Enter choice [1-4] : "))
 
     if(n==1):
         return 3
@@ -71,6 +71,7 @@ def numberOfRounds():
 class Rounds:
 
     def __init__(self):
+        self.m=mainDashboard()
         self.n=numberOfRounds()
 
     def rounds(self):
@@ -78,5 +79,106 @@ class Rounds:
             play()
     
 
+
+def mainDashboard():
+    print("========= RPS-Arena ==========")
+    print("1) Quick Match vs CPU")
+    print("2) Local PvP")
+    print("3) Show Leaderboard")
+    print("4) Reset Leaderboard")
+    print("5) Quit")
+
+    go = int(input("Choose an option [1-5]"))
+
+    if go==1:
+        name = input("Enter you name")
+        print("Choose the difficulty level")
+        print("1) Easy (random)")
+        print("2) Medium (Frequency)")
+        print("3) Hard (Predictive)")
+        difficulty = int(input("Enter choice [1-3]"))
+
+        
+
+
+
+
+
+
 run = Rounds()
 run.rounds()
+'''
+
+class Player:
+    def __init__(self,name="bot"):
+        self.name = name
+        self.score = 0
+
+    def addPoints(self):
+        self.score += 1
+
+class Game:
+    valid_moves = ["rock","paper","scissor"]
+
+    def __init__(self,user,bot):
+        self.user = user
+        self.bot = bot
+        self.draws = 0
+
+    def play_round(self):
+        move = input("Enter your choice (r/p/s) : ").lower()
+        rps = {"r":"rock","p":"paper","s":"scissor"}
+        user_choice = rps.get(move)
+        if move not in rps:
+            print("Invalid input!")
+            return
+        bot_choice = random.choice(self.valid_moves)
+        print(f"{self.user.name} : {user_choice}")
+        print(f"{self.bot.name} : {bot_choice}")
+        
+        if(user_choice==bot_choice):
+            print("It is a draw!")
+            self.draws+=1
+        elif((user_choice=="rock" and bot_choice=="scissor") or 
+             (user_choice=="paper" and bot_choice=="rock") or
+             (user_choice=="scissor" and bot_choice=="paper")):
+            print(f"{self.user.name} wins!")
+            self.user.addPoints()
+        else:
+            print("Bot wins!")
+            self.bot.addPoints()
+
+        self.show_score()
+
+    def show_score(self):
+        print(f"Score => {self.user.name} : {self.user.score} , {self.bot.name} : {self.bot.score}, Draws : {self.draws}")
+
+    def play_series(self):
+        for i in range(self.no_of_rounds()):
+            self.play_round()
+        print("=== Final Score ===")
+        self.show_score()
+
+    def no_of_rounds(self):
+        print("Enter the no of rounds")
+        print("1) Best of 3")
+        print("2) Best of 5")
+        print("3) Best of 7")
+        print("4) Custom choice")
+        n = int(input("Enter choice [1-4] : "))
+        if(n==1):
+            return 3
+        elif(n==2):
+            return 5
+        elif(n==3):
+            return 7
+        elif(n==4):
+            m = int(input("Enter your custom number"))
+            return m
+
+if __name__ == "__main__":
+    user = Player(input("Enter your name"))
+    bot = Player("Bot")
+    game = Game(user, bot)
+    game.play_series()
+        
